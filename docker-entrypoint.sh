79c2570b0a30645db64aad1fac48314e1d94293f
gbridge888 <gbridge888@gmail.com>
2022-11-14 06:29:13 -0800

    U

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! -f /root/sd/pywork/dr_py/app.py ]; then
 	rm -rf .git* base/rules.db
 	echo "App Initialized"
 	echo "Version $(cat js/version.txt)"
-esif [ "$AUTOUPDATE" != 0 ]; then
+elif [ "$AUTOUPDATE" != 0 ]; then
 	mv base/rules.db base/直播.txt /tmp
 	ls -A1 | xargs rm -rf
 	git clone --depth 1 -q ${REPO_URL} .
