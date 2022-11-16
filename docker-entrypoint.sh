18eee6dbd092bbdb7c8d27bdef02d3c8022abdc1
gbridge888 <gbridge888@gmail.com>
2022-11-16 14:35:58 -0800

    u

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set -e
 
-if [ ! -f /root/sd/pywork/dr_py/app.py ]; then
+if [ ! -f /root/sd/pywork/dr_py/app.py ] 
+then
 	git clone --depth 1 -q ${REPO_URL} .
 	rm -rf .git* base/rules.db
 	echo "App Initialized from zero"
 	echo "Version $(cat js/version.txt)"
 else
-	mv base/rules.db base/直播.txt /tmp
+	mv base/rules.db /tmp
 	ls -A1 | xargs rm -rf
 	git clone --depth 1 -q ${REPO_URL} .
 	rm -rf .git*
-	mv -f /tmp/rules.db /tmp/直播.txt base/
+	mv -f /tmp/rules.db base/
 	echo "App Updated"
 	echo "Version $(cat js/version.txt)"
 fi
